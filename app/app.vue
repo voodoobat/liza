11068b34a7c0bca36096a72496c7882069bab70a
Vova Rodin <mynameisvova@yahoo.com>
2025-10-29 16:52:26 +0300

    feat: adds tailwind

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,5 @@
 <template>
-  <div />
+  <div class="container mx-auto">
+    <h1 class="text-2xl text-center py-12">It works</h1>
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    rootAttrs: {
+      id: 'root',
+    },
+  },
+  css: ['~/assets/css/index.css'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })

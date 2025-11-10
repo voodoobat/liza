93281ce6a9673e2c2ffb53ab2e7dafa7ee676a9e
Vova Rodin <mynameisvova@yahoo.com>
2025-11-11 00:02:14 +0300

    fix: removes refresh token on logout

--- server/api/auth/logout.post.ts
+++ server/api/auth/logout.post.ts
@@ -1,4 +1,12 @@
+import { $auth } from '@@/server/services'
+
 export default defineEventHandler(async (event) => {
+  const refreshToken = getCookie(event, 'refresh-token')
+
+  if (refreshToken) {
+    await $auth.delete(refreshToken)
+  }
+
   setCookie(event, 'access-token', '', { httpOnly: true })
   setCookie(event, 'refresh-token', '', { httpOnly: true })
 })
